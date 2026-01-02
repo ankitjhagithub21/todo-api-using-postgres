d7cfa57c117e299bb16ddd1d4b31be8a69c66774
ankit <ajha45075@gmail.com>
2026-01-02 17:44:58 +0530

    setup routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,32 @@
 
 import { Button } from "@/components/ui/button"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import LoginPage from "./pages/LoginPage"
+import RegisterPage from "./pages/RegisterPage"
 
 const App = () => {
+
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element:<HomePage/>
+    },
+     {
+      path:"/login",
+      element:<LoginPage/>
+    },
+
+    {
+      path:"/register",
+      element:<RegisterPage/>
+    },
+
+  ])
   return (
-    <div>
-      <h1 className="text-3xl font-bold">Hello world</h1>
-        <Button>Click me</Button>
-    </div>
+    <>
+    <RouterProvider router={router}/>
+    </>
   )
 }
 
