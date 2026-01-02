0de08d9e5f5b135c4a6e46458300c31d0a906f68
ankit <ajha45075@gmail.com>
2026-01-02 17:42:09 +0530

    setup shadcn ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
 
+import { Button } from "@/components/ui/button"
+
 const App = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold">Hello world</h1>
+        <Button>Click me</Button>
     </div>
   )
 }
